87ae9f959a3ef672266e2da3aa74b6055bd5211e
Siarhei Lipchyk <contact@siarhei.dev>
2022-07-10 03:35:48 +0200

    Add envs to customize ports

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+graphql_port=${GRAPHQL_PORT:-8080}
+api_port=${API_PORT:-9693}
+console_port=${CONSOLE_PORT:-9695}
+
+
 # Workaround for https://github.com/hasura/graphql-engine/issues/2824#issuecomment-801293056
-socat TCP-LISTEN:8080,fork TCP:graphql-engine:8080 &
-socat TCP-LISTEN:9695,fork,reuseaddr,bind=hasura-console TCP:127.0.0.1:9695 &
-socat TCP-LISTEN:9693,fork,reuseaddr,bind=hasura-console TCP:127.0.0.1:9693 &
+socat TCP-LISTEN:${graphql_port},fork TCP:graphql-engine:8080 &
+socat TCP-LISTEN:${console_port},fork,reuseaddr,bind=hasura-console TCP:127.0.0.1:9695 &
+socat TCP-LISTEN:${api_port},fork,reuseaddr,bind=hasura-console TCP:127.0.0.1:9693 &
 
 # Run console if specified
 if [[ -v HASURA_RUN_CONSOLE ]]; then
